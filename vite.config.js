881ced416083ba8d85fe1a54515c5220e18cb7e9
Viktor Minchev <vikitominchev@gmail.com>
2024-09-05 19:51:45 +0300

    push

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/viktorinkov-nike-demo/',  // Base URL for GitHub Pages
+  base: '/viktorinkov-nike-demo/',  // Correct base path for GitHub Pages
   plugins: [react()],
 })
